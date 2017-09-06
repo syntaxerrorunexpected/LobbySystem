caabb5c82a32b8d74f075375bc6f64addbc2ddac
GhostKilllaX <ingomar.mayer1@gmail.com>
2017-09-06 19:42:43 +0200

    yoh

--- src/LobbySystem/main.java
+++ src/LobbySystem/main.java
@@ -8,12 +8,15 @@ import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.event.player.PlayerDropItemEvent;
 import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
+import cn.nukkit.event.player.PlayerRespawnEvent;
 import cn.nukkit.event.player.PlayerTeleportEvent;
 import cn.nukkit.item.Item;
+import cn.nukkit.level.Level;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.tag.CompoundTag;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class main extends PluginBase implements Listener{
@@ -25,35 +28,62 @@ public class main extends PluginBase implements Listener{
         getServer().getPluginManager().registerEvents(this, this);
         this.saveDefaultConfig();
         this.reloadConfig();
+        Level l = this.getServer().getLevelByName(this.getConfig().getString("level"));
+        Player[] player = l.getPlayers().values().toArray(new Player[0]);
+        for(Player pl : player){
+            inv.put(pl, pl.getInventory().getContents());
+            this.putitems(pl, "items");        
+        }
     }
-     
     
-    public void putitems(Player pl){
-        inv.put(pl, pl.getInventory().getContents());
+    @Override
+    public void onDisable(){
+        Level l = this.getServer().getLevelByName(this.getConfig().getString("level"));
+        Player[] player = l.getPlayers().values().toArray(new Player[0]);
+        for(Player pl : player){
+            if(inv.containsKey(pl)){
+                pl.getInventory().clearAll();
+                pl.getInventory().setContents(inv.get(pl));
+            }
+        }
+    }
+      
+    public void putitems(Player pl, String ln){
         pl.getInventory().clearAll();
-        for(String i : this.getConfig().getStringList("items")){
+        if(!this.getConfig().exists(ln)){
+            this.getLogger().error("Error list " + ln + " not found");
+            return;
+        }
+        List<String> l = this.getConfig().getStringList(ln);
+        for(String i : l){
+            int cs = l.indexOf(i) + 9;
             String it[] = i.split("\\|");
-            this.getLogger().debug(Arrays.toString(it));
             if(it.length<3){
-                this.getLogger().error("invalid item syntax... should be \"itemid|slot|name\"");
+                this.getLogger().error("invalid item syntax in list " + ln + " ... should be \"itemid|slot|name(|cmd)\"");
+                continue;
+            } 
+            if(!isInteger(it[0]) || !isInteger(it[1])){
+                this.getLogger().error("invalid itemid or slot in list " + ln + " ... " + it[0] + " or " + it[1] + " is not a number!");
+                continue;
             }
             Item item = new Item(Integer.parseInt(it[0]));
             if(!(item instanceof Item)){
-               this.getLogger().error("invalid itemid... ");
+               this.getLogger().error("invalid itemid in list " + ln + " ... ");
             }
-            int slot = Integer.parseInt(it[1]);
-            if(slot<0 || slot>35){
-                this.getLogger().error("invalid slot... ");
+            int slot = Integer.parseInt(it[1]); 
+            if(slot<1 || slot>9){
+                this.getLogger().error("invalid slot in list " + ln + " ... ");
             }
-            String name = it[2];
-            item = item.setCustomName(name);
-            if(it.length==4){
+            if(it.length>3){
                 CompoundTag nbt = new CompoundTag();
                 nbt.putString("cmd", it[3]);
                 item.setNamedTag(nbt);
             }
-            this.getLogger().debug(item.toString());
+            String name = it[2];
+            name = name.replaceAll("%n", "\n");
+            item = item.setCustomName(name);
             pl.getInventory().addItem(item);
+            pl.getInventory().setHotbarSlotIndex(cs, slot - 1);
         }
     }
     
@@ -61,7 +91,8 @@ public class main extends PluginBase implements Listener{
     public void onjoin(PlayerJoinEvent ev){
         Player pl = ev.getPlayer();
         if(pl.getLevel().getName().equals(this.getConfig().getString("level"))){
-            this.putitems(pl);
+            inv.put(pl, pl.getInventory().getContents());
+            this.putitems(pl, "items");
         } 
     }
     
@@ -79,30 +110,59 @@ public class main extends PluginBase implements Listener{
             if(ev.getFrom().getLevel().getName().equals(this.getConfig().getString("level"))){
                 pl.getInventory().clearAll();
                 pl.getInventory().setContents(inv.get(ev.getPlayer()));
+                inv.remove(pl);
                 return;
             }
             if(ev.getTo().getLevel().getName().equals(this.getConfig().getString("level"))){
-                this.putitems(pl);
+                inv.put(pl, pl.getInventory().getContents());
+                this.putitems(pl, "items");
             }
         }        
     }
     
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
+    public void onquit(PlayerQuitEvent ev){
+        if(inv.containsKey(ev.getPlayer())){
+            ev.getPlayer().getInventory().setContents(inv.get(ev.getPlayer()));
+        }    
+    }
+    
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
+    public void onrip(PlayerRespawnEvent ev){
+        Player pl = ev.getPlayer();       
+        if(pl.getLevel().getName().equals(this.getConfig().getString("level"))){
+            this.putitems(pl, "items");
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
     public void ontab(PlayerInteractEvent ev){
-        if(ev.getPlayer().getLevel().getName().equals(this.getConfig().getString("level"))){
-                String cmd = ev.getItem().getNamedTag().getString("cmd");
-                if(cmd == null){
+        Player pl = ev.getPlayer();
+        if(pl.getLevel().getName().equals(this.getConfig().getString("level"))){
+                Item i = ev.getItem();          
+                if(!i.hasCompoundTag()){
                     return;
                 }
-                cmd = cmd.replaceAll("%p", ev.getPlayer().getName());
+                String cmd = i.getNamedTag().getString("cmd");
+                if(cmd.isEmpty()){
+                    return;
+                }
+                cmd = cmd.replaceAll("%p", pl.getName());
                 if(cmd.startsWith("tpto")){
                     cmd = cmd.replaceFirst("tpto", "");
                     cmd = cmd.replaceFirst(" ", "");
                     String xyz[] = cmd.split(" ");
-                    ev.getPlayer().teleport(new Vector3(Integer.parseInt(xyz[0]), Integer.parseInt(xyz[1]), Integer.parseInt(xyz[0])));
+                    pl.teleport(new Vector3(Integer.parseInt(xyz[0]), Integer.parseInt(xyz[1]), Integer.parseInt(xyz[0])));
+                    return;
+                }
+                if(cmd.startsWith("ilist")){
+                    cmd = cmd.replaceFirst("ilist", "");
+                    String ln = cmd.replaceFirst(" ", "");
+                    this.putitems(pl, ln);
                     return;
                 }
                 this.getServer().dispatchCommand(this.getServer().getConsoleSender(), cmd);
         }
-    }
+    }   
+    public static boolean isInteger(String s){ boolean isValidInteger = false;try{Integer.parseInt(s);isValidInteger = true;}catch (NumberFormatException ex){ }return isValidInteger;}
 }
